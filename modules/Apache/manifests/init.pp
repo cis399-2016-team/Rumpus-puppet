dbb00ec3a9d6676d136b9fc425dd140a4025d332
root <root@ip-10-0-7-43.us-west-2.compute.internal>
2016-07-18 22:51:17 +0000

    Apache

---
+++ modules/Apache/manifests/init.pp
@@ -0,0 +1,27 @@
+class Apache {
+	package {
+		"apache2" ensure => installed;
+		}
+	file {
+		"/etc/apache2/apache2.conf":
+		source  =>  [
+			"puppet:///modules/apache2/$hostname/apache2.conf",
+			"puppet:///modules/apache2/apache2.conf",
+		],
+		mode => 444,
+		owner => root,
+		group => root,
+		require => Package["apache2"],
+	}
+	service {
+		"apache2":
+		enable =>true,
+		ensure=>running,
+		hasstatus=> true,
+		hasrestart => true,
+		require => [Package["apache2"],
+			File["/etc/apache2.conf"] ],
+
+		subscribe => File["/etc/apache2.conf"],
+	}
+}
