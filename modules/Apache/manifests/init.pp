7eb969ebbf7a55c4e71b1abcbc7a0d017040c8d2
root <root@ip-10-0-7-76.us-west-2.compute.internal>
2016-07-21 22:35:46 +0000

    changing the module because it wasn't working as it was

--- modules/Apache/manifests/init.pp
+++ modules/Apache/manifests/init.pp
@@ -2,25 +2,21 @@ class Apache {
 	package {
 		"apache2": ensure => installed;
 		}
-	file {
-		"/etc/apache2/apache2.conf":
-		source  =>  [
-			"puppet:///modules/Apache/apache2.conf",
-		],
-		mode => 444,
+	file { "/etc/apache2/apache2.conf":
+		source  =>  "puppet:///modules/Apache/apache2.conf",
+		mode => 644,
 		owner => root,
 		group => root,
 		require => Package["apache2"],
 	}
-	service {
-		"apache2":
-		enable =>true,
-		ensure=>running,
-		hasstatus=> true,
+	service { "apache2":
+		enable => true,
+		ensure => running,
+		hasstatus => true,
 		hasrestart => true,
 		require => [Package["apache2"],
-			File["/etc/apache2.conf"] ],
+			File["/etc/apache2/apache2.conf"] ],
 
-		subscribe => File["/etc/apache2.conf"],
+		subscribe => File["/etc/apache2/apache2.conf"],
 	}
 }
