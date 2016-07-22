b5351500dddd04db2a9a79f67f281f59a15757df
root <root@ip-10-0-7-76.us-west-2.compute.internal>
2016-07-22 20:00:33 +0000

    changing directory names to match module class name

---
+++ modules/apache/manifests/init.pp
@@ -0,0 +1,22 @@
+class apache{
+	package{
+		"apache2": ensure => installed;
+		}
+	file{ "/etc/apache2/apache2.conf":
+		source  =>  "puppet:///modules/Apache/apache2.conf",
+		mode => 644,
+		owner => root,
+		group => root,
+		require => Package["apache2"],
+	}
+	service{ "apache2":
+		enable => true,
+		ensure => running,
+		hasstatus => true,
+		hasrestart => true,
+		require => [Package["apache2"],
+			File["/etc/apache2/apache2.conf"] ],
+
+		subscribe => File["/etc/apache2/apache2.conf"],
+	}
+}
