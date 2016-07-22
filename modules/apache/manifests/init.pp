f5d9c8166514e968585ba98d79483231d01521d4
root <root@ip-10-0-7-43.us-west-2.compute.internal>
2016-07-22 23:45:38 +0000

    adding html files to the apache module

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -2,6 +2,7 @@ class apache{
 	package{
 		"apache2": ensure => installed;
 		}
+
 	file{ "/etc/apache2/apache2.conf":
 		source  =>  "puppet:///modules/apache/apache2.conf",
 		mode => 644,
@@ -9,6 +10,13 @@ class apache{
 		group => root,
 		require => Package["apache2"],
 	}
+
+	file{ "/var/www/html":
+		source => [puppet:///modules/apache/html"],
+		mode => 644,
+		recurse => true,
+	}
+
 	service{ "apache2":
 		enable => true,
 		ensure => running,
