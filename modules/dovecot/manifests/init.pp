5954932c7dd9ba798af416c6feb783a0284fa87b
root <root@ip-10-0-7-76.us-west-2.compute.internal>
2016-08-05 18:34:57 +0000

    Adding dovecot module

---
+++ modules/dovecot/manifests/init.pp
@@ -0,0 +1,33 @@
+class dovecot{
+	package { "dovecot-imapd":
+	    	ensure	=> installed,
+	    }
+	package { "dovecot-mysql":
+	    	ensure	=> installed,
+	    }
+
+	file {"/etc/dovecot/dovecot.conf":
+            mode    => 0644,
+            owner   => "root",
+            group   => "root",
+            source  => "puppet:///modules/dovecotmod/dovecot/dovecot.conf",
+            notify  => Service["dovecot"],
+        }
+
+        file { "/etc/dovecot/conf.d/":
+            ensure  => directory,
+            recurse => true,
+            purge   => false,
+            force   => true,
+            owner   => "root",
+            group   => "root",
+            mode    => 0644,
+            source  => "puppet:///modules/dovecotmod/dovecot/conf.d",
+        }
+        service {"dovecot":
+            enable    => true,
+            ensure    => running,
+            require   => [ Package["dovecot-imapd"], Package["dovecot-mysql"], File["/etc/dovecot/dovecot.conf"] ],
+            subscribe => File["/etc/dovecot/dovecot.conf"],
+        }
+}
